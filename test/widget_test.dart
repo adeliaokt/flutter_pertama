2f1104737c56195a072c682b4af33a0cdb6e73c0
Adelia Oktaviani <adeliaaa.ok@gmail.com>
2022-04-12 14:16:08 +0700

    commit null safety

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -103,6 +103,18 @@ void main() {
   mahasiswafk = "beben";
 
   print(mahasiswafk);
+
+  //null safety
+  // ? ! late
+  // ? digunakan dia boleh null
+//   String? jurusan;
+// untuk diisi nanti
+  late String jurusanns;
+
+  jurusanns = "Teknik Informatika";
+//   jurusan = "TI";
+  // ! memaksa untuk dijalankan /  yakin ada datanya
+  print(jurusanns.length);
 }
 
 //function
